c2ebfc216b04b891427308de8b735c05350734b3
Greg OToole <gto1@psu.edu>
2024-06-04 13:55:34 -0400

    Ran through Coder Upgrade.

--- js/hidden_nodes.js
+++ js/hidden_nodes.js
@@ -1,13 +1,13 @@
 (function ($) {
 
-Drupal.behaviors.hiddenNodeFieldsetSummaries = {
+Backdrop.behaviors.hiddenNodeFieldsetSummaries = {
   attach: function (context) {
-    $('fieldset#edit-hidden-nodes', context).drupalSetSummary(function (context) {
+    $('fieldset#edit-hidden-nodes', context).backdropSetSummary(function (context) {
       var hid = $('input', context).is(':checked');
 
       return hid ?
-        Drupal.t('Hidden') :
-        Drupal.t('Normal (not hidden)'); 
+        Backdrop.t('Hidden') :
+        Backdrop.t('Normal (not hidden)'); 
     });
   }
 };
